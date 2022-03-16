672f07a78818c5939cd4b47cd42058cb0bf78d10
Jan Erik Hanssen <jhanssen@gmail.com>
2022-03-15 18:26:41 -0700

    make it be possible to have multiple watches with separate optional filters

--- src/index.js
+++ src/index.js
@@ -2,11 +2,41 @@ import { Dropbox } from "dropbox";
 import { default as Options } from "@jhanssen/options";
 import { watch } from "chokidar";
 import { readFile } from "node:fs/promises";
+import { join as pathJoin } from "node:path";
 
 const options = Options("video-upload-dropbox");
 const accessToken = options("access-token");
-const watchDirectory = options("watch-dir");
+const watches = options.json("watches");
 const uploadTimeout = options.int("upload-timeout", 5000);
+
+if (!(watches instanceof Array)) {
+    console.error("watches needs to be an array");
+    process.exit(1);
+}
+for (const w of watches) {
+    if (typeof w !== "object") {
+        console.error("watch needs to be an object");
+        process.exit(1);
+    }
+    if (typeof w.dir !== "string") {
+        console.error("watch.dir needs to be a string");
+        process.exit(1);
+    }
+    if (w.filter !== undefined) {
+        if (typeof w.filter !== "string") {
+            console.error("watch.filter needs to be undefined or a string");
+            process.exit(1);
+        } else {
+            // make filter be a regex
+            try {
+                w.filter = new RegExp(w.filter);
+            } catch (e) {
+                console.error("watch.filter not a valid regexp", e.message);
+                process.exit(1);
+            }
+        }
+    }
+}
 
 let timer = undefined;
 let files = [];
@@ -18,15 +48,28 @@ function removeSlash(p) {
 }
 
 function stripPath(p) {
-    if (p.indexOf(watchDirectory) === 0)
-        return removeSlash(p.substr(watchDirectory.length));
+    for (const w of watches) {
+        if (p.indexOf(w.dir) === 0) {
+            let ls = w.dir.lastIndexOf("/");
+            if (ls === w.dir.length - 1 && ls > 0) {
+                ls = w.dir.lastIndexOf("/", ls - 1);
+            }
+            if (ls !== -1)
+                return removeSlash(p.substr(ls));
+            return removeSlash(p.substr(w.dir.length));
+        }
+    }
     return removeSlash(p);
 }
 
-async function uploadFiles(subfiles) {
+async function uploadFiles(subfiles, filter) {
     const dbx = new Dropbox({ accessToken });
 
     for (let f of subfiles) {
+        if (filter !== undefined && !filter.test(f)) {
+            console.log("skipping", stripPath(f));
+            continue;
+        }
         const contents = await readFile(f);
         try {
             const ret = await dbx.filesUpload({ path: "/" + stripPath(f), contents: contents });
@@ -37,7 +80,7 @@ async function uploadFiles(subfiles) {
         } catch (e) {
             if (e.code === "ECONNREFUSED") {
                 // retry
-                addUploadFile(f);
+                addUploadFile(f, filter);
             } else {
                 throw e;
             }
@@ -45,7 +88,7 @@ async function uploadFiles(subfiles) {
     }
 }
 
-function addUploadFile(file) {
+function addUploadFile(file, filter) {
     files.push(file);
     if (timer !== undefined)
         clearTimeout(timer);
@@ -53,7 +96,7 @@ function addUploadFile(file) {
         const f = files;
         files = [];
 
-        uploadFiles(f).then(() => {
+        uploadFiles(f, filter).then(() => {
         }).catch(e => {
             console.error(e);
         });
@@ -62,11 +105,13 @@ function addUploadFile(file) {
     }, uploadTimeout);
 }
 
-console.log("watching", watchDirectory);
+console.log("watching", watches);
 
-watch(watchDirectory, { ignoreInitial: true }).on("all", (event, path) => {
-    if (event === "add") {
-        console.log("file added", path);
-        addUploadFile(path);
-    }
-});
+for (const w of watches) {
+    watch(w.dir, { ignoreInitial: true }).on("all", (event, path) => {
+        if (event === "add") {
+            console.log("file added", path);
+            addUploadFile(path, w.filter);
+        }
+    });
+}
